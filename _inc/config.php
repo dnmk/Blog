cc5d120717b6442255883a53d6b747eace0753a9
nate1k <matousek.dominik@icloud.com>
2017-02-28 23:55:28 +0100

    Adding APP

---
+++ _inc/config.php
@@ -0,0 +1,30 @@
+<?php
+
+// show all errors
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(-1);
+
+
+// require stuff
+if( !session_id() ) @session_start();
+require_once 'vendor/autoload.php';
+
+
+// global variables
+$base_url = 'http://localhost:8888/todoapp';
+
+
+// connect to db
+$database = new medoo([
+	'database_type' => 'mysql',
+	'database_name' => 'todo',
+	'server'        => 'localhost',
+	'username'      => 'root',
+	'password'      => 'root',
+	'charset'       => 'utf8'
+]);
+
+
+// global functions
+require_once 'functions.php';
