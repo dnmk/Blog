b1ece39bc924c93226c27e439061ea00402a1626
nate1k <matousek.dominik@icloud.com>
2017-03-03 11:02:57 +0100

    New Style

--- home.php
+++ home.php
@@ -12,32 +12,35 @@ include_once '_partials/header.php'
 
 		<?php if ( count( $results ) ) : foreach ($results as $post) : ?>
 
-			<article id="pos-<?= $post->id ?>" class="post">
-				<header class="post-header">
-					<h2>
-						<a href="<?= BASE_URL ?>/post/<?= $post->id ?>/<?= plain( $post->slug ) ?>">
-							<?= plain($post->title) ?>
-						</a>
-					</h2>
+				<div class="row">
+					<div class="col-sm-6 col-sm-offset-3">
+						<article id="pos-<?= $post->id ?>" class="post">
+							<header class="post-header">
+								<h2>
+									<a href="<?= BASE_URL ?>/post/<?= $post->id ?>/<?= plain( $post->slug ) ?>">
+										<?= plain($post->title) ?>
+									</a>
+								</h2>
 
-					<time datetime="<?= date( 'Y-m-d', strtotime( $post->created_at ) ) ?>">
-						<small> / <?= date('j M Y, G:i', strtotime( $post->created_at ) ) ?></small>
-					</time>
-				</header>
+								<time datetime="<?= date( 'Y-m-d', strtotime( $post->created_at ) ) ?>">
+									<small> / <?= date('j M Y, G:i', strtotime( $post->created_at ) ) ?></small>
+								</time>
+							</header>
 
-				<div class="post-content">
-					<p>
-						<?= word_limiter(plain($post->text), 20) ?>
-					</p>
+							<div class="post-content">
+								<p>
+									<?= word_limiter(plain($post->text), 20) ?>
+								</p>
+							</div>
+
+							<div class="footer post-footer">
+								<a class="read-more" href="<?= BASE_URL ?>/post/<?= $post->id ?>/<?= plain( $post->slug ) ?>">
+									// read more >>
+								</a>
+							</div>
+						</article>
+					</div>
 				</div>
-
-				<div class="footer post-footer">
-					<a class="read-more" href="<?= BASE_URL ?>/post/<?= $post->id ?>/<?= plain( $post->slug ) ?>">
-						read more
-					</a>
-				</div>
-			</article>
-
 
 
 		<?php endforeach; else: ?>
